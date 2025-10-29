5b05cc4b9087004260559d193e02a7e13b6c0f89
sayna <saenaanbari@gmail.com>
2025-10-29 04:15:19 -0700

    add custom font for body in style.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import { Factor , UpdateCartToal } from './components/factor/factor'
 import { RenderItems } from './components/menu/menu'
 import './style.css'
 
 let app = document.getElementById("app")
-app.append(RenderItems())
+app.classList.add(
+  "p-4",
+  "flex",
+  "flex-col",
+  "gap-5",
+  "lg:flex-row",
+  "lg:gap-10"
+);
+app.append(RenderItems() , Factor())
+UpdateCartToal()
